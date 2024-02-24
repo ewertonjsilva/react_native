2669e6624fdc93d099a3a021a495908946843416
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-02-23 22:49:08 -0300

    aula props

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Props from './src/ex_2';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Props mensagem={'Teste'} />
+      <Props nome={'Ewerton'} />
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/ex_2/index.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'; 
+
+export default function Props ({mensagem}) {
+    return(
+        <View>
+            <Text>{mensagem}</Text>
+        </View>
+    );
+}
