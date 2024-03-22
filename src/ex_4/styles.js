987716d06bbdaaabc0ad3d51a0d78986d31364ba
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-03-22 20:39:31 -0300

    aula 22-03

--- src/ex_4/styles.js
+++ src/ex_4/styles.js
@@ -13,7 +13,22 @@ const styles = StyleSheet.create({
         fontSize: 30, 
         fontWeight: 'bold', 
         color: '#b71c1c', 
-    },
+    }, 
+    txtSaida: {
+        margin: 20, 
+        fontSize: 25, 
+        color: '#e53935', 
+    }, 
+    txtEntrada: {
+        borderWidth: 3, 
+        textAlign: 'center', 
+        fontSize: 22, 
+        borderColor: '#b71c1c', 
+        height: 40, 
+        width: '70%', 
+        color: '#e53935', 
+        borderRadius: 10, 
+    }, 
 }); 
 
 export default styles;
