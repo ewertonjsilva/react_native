ad2035e66bf30053f22e8f7aebaf8e5c04999751
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2024-05-02 19:27:34 -0300

    caulculadora

--- src/ex_7/button/index.js
+++ src/ex_7/button/index.js
@@ -2,9 +2,16 @@ import { Text, Pressable } from 'react-native';
 import styles from './styles';
 
 export default function Button(props) {
+
+    const stylesButton = [styles.button];
+
+    if (props.double) stylesButton.push(styles.buttonDouble);
+    if (props.triple) stylesButton.push(styles.buttonTriple);  
+    if (props.operation) stylesButton.push(styles.operationButton); 
+
     return(
-        <Pressable onPress={props.onClick}>
-            <Text style={styles.button}>{props.label}</Text>
+        <Pressable onPress={() => props.onClick(props.label)}>
+            <Text style={stylesButton}>{props.label}</Text>
         </Pressable>
     );
 }
--- src/ex_7/index.js
+++ src/ex_7/index.js
@@ -1,36 +1,89 @@
 import { useState } from 'react';
-import { View } from 'react-native'; 
+import { View } from 'react-native';
 
 import Button from './button';
 import Display from './display';
 
 import styles from './styles';
 
-export default function Exemplo7 () { 
+export default function Exemplo7() {
 
     const [displayValue, setDisplayValue] = useState('0');
+    const [clearDisplay, setClearDisplay] = useState(false);
+    const [operation, setOperation] = useState(null);
+    const [values, setValues] = useState([0, 0]);
+    const [current, setCurrent] = useState(0);
 
-    return(
+    function addDigit(n) {
+
+        const limpaTela = displayValue === '0' || clearDisplay;
+
+        if (n === '.' && !limpaTela && displayValue.includes('.')) {
+            return;
+        }
+
+        const currentValue = limpaTela ? '' : displayValue;
+        const valorAtualizar = currentValue + n;
+
+        if (n !== '.') {
+            const novoValor = parseFloat(valorAtualizar);
+            const atualizaValor = values;
+            atualizaValor[current] = novoValor;
+            setValues(atualizaValor);
+        }
+
+        setDisplayValue(valorAtualizar);
+        setClearDisplay(false);
+
+    }
+
+    function clearMemory() {
+        setDisplayValue('0');
+        setClearDisplay(false);
+        setOperation(null);
+        setValues([0, 0]);
+        setCurrent(0);
+    }
+
+    function definieOperacao(operacao) {
+        if (current === 0) {
+            setOperation(operacao);
+            setCurrent(1);
+            setClearDisplay(true);
+        } else {
+            const sinalIgual = operacao === '=';
+            const valores = values;
+
+            valores[0] = eval(valores[0] + operation + valores[1]);
+            valores[1] = 0;
+            setDisplayValue(values[0].toString());
+            setOperation(sinalIgual ? 0 : 1);
+            setClearDisplay(true);
+            setValues(valores);
+        }
+    }
+
+    return (
         <View style={styles.container}>
             <Display value={displayValue} />
             <View style={styles.buttons}>
-                <Button label='AC' />
-                <Button label='/' />
-                <Button label='7' />
-                <Button label='8' />
-                <Button label='9' />
-                <Button label='*' />
-                <Button label='4' />
-                <Button label='5' />
-                <Button label='6' />
-                <Button label='-' />
-                <Button label='1' />
-                <Button label='2' />
-                <Button label='3' />
-                <Button label='+' />
-                <Button label='0' />
-                <Button label='.' />
-                <Button label='=' />
+                <Button label='AC' triple onClick={() => clearMemory()} />
+                <Button label='/' operation onClick={definieOperacao} />
+                <Button label='7' onClick={addDigit} />
+                <Button label='8' onClick={addDigit} />
+                <Button label='9' onClick={addDigit} />
+                <Button label='*' operation onClick={definieOperacao} />
+                <Button label='4' onClick={addDigit} />
+                <Button label='5' onClick={addDigit} />
+                <Button label='6' onClick={addDigit} />
+                <Button label='-' operation onClick={definieOperacao} />
+                <Button label='1' onClick={addDigit} />
+                <Button label='2' onClick={addDigit} />
+                <Button label='3' onClick={addDigit} />
+                <Button label='+' operation onClick={definieOperacao} />
+                <Button label='0' double onClick={addDigit} />
+                <Button label='.' onClick={addDigit} />
+                <Button label='=' operation onClick={definieOperacao} />
             </View>
         </View>
     );
